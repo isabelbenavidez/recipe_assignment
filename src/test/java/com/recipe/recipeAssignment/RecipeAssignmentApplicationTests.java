8cbced00fc4a5d591e306a0c68c3a465b20125a5
isabelbenavidez <isabelbenavidez@gmail.com>
2022-03-18 15:25:01 -0500

    Creación de repositorio en git hub

---
+++ src/test/java/com/recipe/recipeAssignment/RecipeAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recipe.recipeAssignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
